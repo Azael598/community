3081443d3c93a5815c7fd253aecaaebd422a24ef
Azael598 <987819317@qq.com>
2020-08-08 22:40:30 +0800

    integrate mybatis generator

--- src/main/java/life/john/community/CommunityApplication.java
+++ src/main/java/life/john/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package life.john.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("life.john.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
