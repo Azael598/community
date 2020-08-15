d2a90cf263beafde281f3639eac360db82d110a3
Azael598 <987819317@qq.com>
2020-08-15 15:12:38 +0800

    complete the main reply function

--- src/main/java/life/john/community/controller/CommentController.java
+++ src/main/java/life/john/community/controller/CommentController.java
@@ -1,6 +1,6 @@
 package life.john.community.controller;
 
-import life.john.community.dto.CommentDTO;
+import life.john.community.dto.CommentCreateDTO;
 import life.john.community.dto.ResultDTO;
 import life.john.community.exception.CustomizeErrorCode;
 import life.john.community.mapper.CommentMapper;
@@ -26,16 +26,16 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment",method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request){
         User user = (User) request.getSession().getAttribute("user");
         if (user == null){
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
         Comment comment = new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setCommentator(user.getId());
--- src/main/java/life/john/community/controller/QuestionController.java
+++ src/main/java/life/john/community/controller/QuestionController.java
@@ -1,6 +1,9 @@
 package life.john.community.controller;
 
+import life.john.community.dto.CommentCreateDTO;
+import life.john.community.dto.CommentDTO;
 import life.john.community.dto.QuestionDTO;
+import life.john.community.service.CommentService;
 import life.john.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,17 +11,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private CommentService commentService;
+
     @GetMapping("/question/{id}")
     public String question(@PathVariable(name = "id") Long id, Model model){
         QuestionDTO questionDTO = questionService.getById(id);
+        List<CommentDTO> comments = commentService.listByQuestionId(id);
         questionService.incView(id);
         model.addAttribute("question",questionDTO);
+        model.addAttribute("comments",comments);
         return "question";
     }
 }
