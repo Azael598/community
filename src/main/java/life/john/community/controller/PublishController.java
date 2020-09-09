f0a42fbe2b5c0fdd80db54f67d52720ba76079d6
Azael598 <987819317@qq.com>
2020-09-09 22:45:47 +0800

    normalize the tags

--- src/main/java/life/john/community/controller/PublishController.java
+++ src/main/java/life/john/community/controller/PublishController.java
@@ -1,9 +1,11 @@
 package life.john.community.controller;
 
+import life.john.community.cache.TagCache;
 import life.john.community.dto.QuestionDTO;
 import life.john.community.model.Question;
 import life.john.community.model.User;
 import life.john.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,12 +30,14 @@ public class PublishController {
         model.addAttribute("description",question.getDescription());
         model.addAttribute("tag",question.getTag());
         model.addAttribute("id",question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model){
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -48,6 +52,7 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
+        model.addAttribute("tags", TagCache.get());
         if(title == null||title == ""){
             model.addAttribute("error","标题不能为空");
             return "publish";
@@ -58,6 +63,11 @@ public class PublishController {
         }
         if (tag == null || tag == ""){
             model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)){
+            model.addAttribute("error","输入非法标签"+invalid);
             return "publish";
         }
 
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -64,30 +64,30 @@ function collapseComments(e) {
             comments.addClass("in");
             e.setAttribute("data-collapse", "in");
             e.classList.add("active");
-        }else {
+        } else {
             $.getJSON("/comment/" + id, function (data) {
                 $.each(data.data.reverse(), function (index, comment) {
-                    var mediaLeftElement = $("<div/>",{
-                        "class":"media-left"
-                    }).append($("<img/>",{
-                        "class":"media-object img-rounded",
-                        "src":comment.user.avatarUrl
+                    var mediaLeftElement = $("<div/>", {
+                        "class": "media-left"
+                    }).append($("<img/>", {
+                        "class": "media-object img-rounded",
+                        "src": comment.user.avatarUrl
                     }));
-                    var mediaBodyElement = $("<div/>",{
-                        "class":"media-body"
-                    }).append($("<h5/>",{
-                        "class":"media-heading",
-                        "html":comment.user.name
-                    })).append($("<div/>",{
-                        "html":comment.content
-                    })).append($("<div/>",{
-                        "class":"menu"
-                    })).append($("<span/>",{
-                        "class":"pull-right",
-                        "html":moment(comment.gmtCreate).format('YYYY-MM-DD')
+                    var mediaBodyElement = $("<div/>", {
+                        "class": "media-body"
+                    }).append($("<h5/>", {
+                        "class": "media-heading",
+                        "html": comment.user.name
+                    })).append($("<div/>", {
+                        "html": comment.content
+                    })).append($("<div/>", {
+                        "class": "menu"
+                    })).append($("<span/>", {
+                        "class": "pull-right",
+                        "html": moment(comment.gmtCreate).format('YYYY-MM-DD')
                     }));
-                    var mediaElement = $("<div/>",{
-                        "class":"media"
+                    var mediaElement = $("<div/>", {
+                        "class": "media"
                     }).append(mediaLeftElement).append(mediaBodyElement);
                     var commentElement = $("<div/>", {
                         "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments",
@@ -100,4 +100,24 @@ function collapseComments(e) {
             });
         }
     }
+}
+
+function showSelectTag() {
+    $("#select-tag").show();
+}
+
+function selectTag(e) {
+    var value = e.getAttribute("data-tag");
+    var previous = $("#tag").val();
+    if (previous.indexOf(value) == -1) {
+        if (previous) {
+            $("#tag").val(previous + ',' + value);
+        } else {
+            $("#tag").val(value);
+        }
+    }
+}
+
+window.onload = () => {
+    $("#select-tag").hide();
 }
