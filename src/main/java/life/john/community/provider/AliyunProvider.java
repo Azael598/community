1c361ac3ba8403f8e257c2640262632f8c3024ee
Azael598 <987819317@qq.com>
2020-09-19 11:26:09 +0800

    store the uploaded files by aliyun oss

---
+++ src/main/java/life/john/community/provider/AliyunProvider.java
@@ -0,0 +1,59 @@
+package life.john.community.provider;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class AliyunProvider {
+
+    @Value("${aliyun.accesskey.id}")
+    String accessKeyId;
+    @Value("${aliyun.accesskey.secret}")
+    String accessKeySecret;
+
+    public String upload(InputStream inputStream, String fileName) {
+        String generatedFileName;
+        String[] filePaths = fileName.split("\\.");
+        if (filePaths.length > 1) {
+            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
+        } else {
+            return null;
+        }
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("community598", generatedFileName, inputStream);
+        Date expiration = new Date(new Date().getTime() + 3600L * 1000 * 24 * 365 * 10);
+        URL url = ossClient.generatePresignedUrl("community598",generatedFileName,expiration);
+// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+// ObjectMetadata metadata = new ObjectMetadata();
+// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+// metadata.setObjectAcl(CannedAccessControlList.Private);
+// putObjectRequest.setMetadata(metadata);
+
+// 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+        return url.toString();
+    }
+
+}
