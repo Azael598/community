d6078ba34cdf9dc5b8a41cfc5120a831e28572bd
Azael598 <987819317@qq.com>
2020-09-26 17:50:38 +0800

    finish the hotTags and add the scheduler

--- src/main/java/life/john/community/controller/indexController.java
+++ src/main/java/life/john/community/controller/indexController.java
@@ -1,6 +1,7 @@
 package life.john.community.controller;
 
 
+import life.john.community.cache.HotTagCache;
 import life.john.community.dto.PaginationDTO;
 import life.john.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class indexController {
 
+    @Autowired
+    private HotTagCache hotTagCache;
 
     @Autowired
     private QuestionService questionService;
@@ -21,11 +26,15 @@ public class indexController {
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size,
-                        @RequestParam(name = "search", required = false) String search) {
+                        @RequestParam(name = "search", required = false) String search,
+                        @RequestParam(name = "tag", required = false) String tag) {
 
-        PaginationDTO pagination = questionService.list(search, page, size);
+        PaginationDTO pagination = questionService.list(search, tag, page, size);
+        List<String> tags = hotTagCache.getHots();
         model.addAttribute("pagination", pagination);
-        model.addAttribute("search",search);
+        model.addAttribute("search", search);
+        model.addAttribute("tags", tags);
+        model.addAttribute("tag",tag);
         return "index";
     }
 }
--- src/main/java/life/john/community/service/QuestionService.java
+++ src/main/java/life/john/community/service/QuestionService.java
@@ -31,7 +31,7 @@ public class QuestionService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
-    public PaginationDTO list(String search,Integer page, Integer size) {
+    public PaginationDTO list(String search, String tag, Integer page, Integer size) {
         if (StringUtils.isNotBlank(search)){
             String[] tags = StringUtils.split(search, " ");
             search = Arrays.stream(tags).collect(Collectors.joining("|"));
@@ -42,6 +42,7 @@ public class QuestionService {
         Integer totalPage;
         QuestionQueryDTO questionQueryDTO = new QuestionQueryDTO();
         questionQueryDTO.setSearch(search);
+        questionQueryDTO.setTag(tag);
         Integer totalCount = questionExtMapper.countBySearch(questionQueryDTO);
 
         if (totalCount%size == 0){
